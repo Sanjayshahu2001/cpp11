42a4255d1f8268c3040a9440d1c237acb279aa53
SANJAY SHAHU <sanjayshahu2647@gmail.com>
2025-07-09 16:40:52 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,9 @@
 #include <iostream>
-using namespace std;
-class PrintNumber
-{
-public:
-    int i;
-    string c;
-    float f;
-    PrintNumber(int x,string y,float z)
-    {
-        i=x;
-        c=y;
-        f=z;
 
-    }
-    void show()
-    {
-        cout<<"\nNumber: "<<i<<"\nName: "<<c<<"\nMark: "<<f<<endl;
-    }
-};
+using namespace std;
+
 int main()
-{    int I;
-    float F;
-    string s;
-    cout<<"Enter Roll:"<<endl;
-    cin>>I;
-    cout<<"Enter Name:"<<endl;
-    cin>>s;
-    cout<<"Enter marks:"<<endl;
-    cin>>F;
-    PrintNumber p(I,s,F);
-    p.show();
+{
+    cout << "Hello world!" << endl;
     return 0;
 }
