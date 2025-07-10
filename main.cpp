77fea39c1ec45b315fb39298ab9ed067a4b62f00
SANJAY SHAHU <sanjayshahu2647@gmail.com>
2025-07-10 18:52:57 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 
 using namespace std;
+class Area
+{
+    int a,b;
+
+    void setDim()
+    {
+        cout<<"Enter Length And Breadth Of a Rectangle:"<<endl;
+        cin>>a>>b;
+
+    }
+    void getArea()
+    {
+        cout<<"Area of Rectangle is :"<<a*b<<endl;
+
+    }
+};
 
 int main()
 {
+    Area a;
+    setDim();
+    getArea();
     cout << "Hello world!" << endl;
     return 0;
 }
